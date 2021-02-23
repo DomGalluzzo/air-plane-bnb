047a09a91662766008fa740c50d653742720fe04
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-23 11:19:20 -0500

    added comma to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/planes', to: 'planes#index'
   get '/planes/new', to: 'planes#new'
   post '/planes', to: 'planes#create'
-  get '/planes/:id' to: 'planes#show'
+  get '/planes/:id', to: 'planes#show'
 
 end
