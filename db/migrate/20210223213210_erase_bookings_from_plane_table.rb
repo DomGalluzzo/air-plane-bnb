446ed1f0d8c481f616eec57ba21ea403277e3f6c
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-23 16:46:47 -0500

    erase bookings from planes table

--- db/migrate/20210223213210_erase_bookings_from_plane_table.rb
+++ db/migrate/20210223213210_erase_bookings_from_plane_table.rb
@@ -1,5 +1,5 @@
 class EraseBookingsFromPlaneTable < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :planes, bookings_id
+  def self.down
+    remove_column :planes, :bookings_id
   end
 end
