a6d8eaa1f0858453fb4751d26ae0b8f412987e55
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-22 20:28:33 -0500

    planes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/planes', to: 'planes#index'
+  get '/planes/new', to: 'planes#new'
+  post '/planes', to: 'planes#create'
+  get '/planes/:id' to: 'planes#show'
+
 end
