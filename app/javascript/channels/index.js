957b44ec877ad6b17477886dc22e50cae79cdd4f
cristhian vargas <cristhian.a.vargas@gmail.com>
2021-02-24 00:40:45 -0500

    landing page

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,65 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+$(function () {
+  $(window).on('scroll', function () {
+      if ( $(window).scrollTop() > 10 ) {
+          $('.navbar').addClass('active');
+      } else {
+          $('.navbar').removeClass('active');
+      }
+  });
+});
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach((panel) =>{
+        panel.classList.remove('active')
+    })
+}
+
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = ["Money", "Time",];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if(charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay)
+    }
+    else{
+        //erase
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase(){
+    if(charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        textArrayIndex++;
+        if(textArrayIndex>=textArray.length) textArrayIndex=0
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(type, newTextDelay + 250);
+});
