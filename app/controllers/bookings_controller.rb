3db6bae4efe19fb8df30509dfaa854ed4f1ad8a1
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-24 16:01:40 -0500

    search and index no longer requires login, spelling fix in BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,18 @@
 class BookingsController < ApplicationController
-    def new
-        @plane = Plane.find(params[:plane_id])
-        @booking = Boking.new
-    end
+	def new
+		@plane = Plane.find(params[:plane_id])
+		@booking = Booking.new
+	end
 
-    def create
-        @booking = Booking.new(booking_params)
-        @plane = Plane.find(params[:plane_id])
-        @booking.plane = @plane
-    end
+	def create
+		@booking = Booking.new(booking_params)
+		@plane = Plane.find(params[:plane_id])
+		@booking.plane = @plane
+	end
 
-    private
-    def booking_params
-        params.require(:booking).permit(:destination, :user_id, :plane_id)
-    end
+  private
+
+	def booking_params
+		params.require(:booking).permit(:destination, :user_id, :plane_id)
+	end
 end
